2abeac588c776fd26e4afb8ecf2463f3c817980b
Torque (Tareq) El Dandachi <tareqdandachi@gmail.com>
2022-10-15 01:43:48 -0400

    init makefile

---
+++ LTspiceDrawer/drawer.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+ 
+using namespace std;
+
+class Circuit {
+   
+   
+
+}
+
+int main(void) {
+   
+   
+
+   return 0;
+}
---
+++ LTspiceDrawer/tests.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main(void) {
+
+    std::cout << "\n --- Tests Began --- \n\n\n";
+    return 0;
+
+}
